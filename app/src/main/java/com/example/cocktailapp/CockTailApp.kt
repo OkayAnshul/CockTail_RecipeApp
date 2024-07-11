462b381aed51e31e787b355922fc6063b0b6b196
Anshul Kumar <anshulisokay@gmail.com>
2024-07-11 11:40:01 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/cocktailapp/CockTailApp.kt
@@ -0,0 +1,10 @@
+package com.example.cocktailapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CockTailApp(modifier: Modifier=Modifier)
+{
+
+}
