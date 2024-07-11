e14ba6ae732548fca6554b6a6b99edad19665585
Anshul Kumar <anshulisokay@gmail.com>
2024-07-11 13:49:57 +0530

    update

--- app/src/main/java/com/example/cocktailapp/CockTailApp.kt
+++ app/src/main/java/com/example/cocktailapp/CockTailApp.kt
@@ -1,10 +1,35 @@
 package com.example.cocktailapp
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun CockTailApp(modifier: Modifier=Modifier)
+fun CockTailApp(modifier: Modifier=Modifier) {
+    val viewModel:DataViewModel= viewModel()
+    val viewState by viewModel.categoryState.value
+LazyColumn(){
+    items(viewState.categoryList){
+        category ->
+    }
+}
+}
+@Composable
+fun showCategory(category: category)
 {
-
+        Card(onClick = {}
+        ){
+            Text(text = category.strCategory)
+        }
 }
--- app/src/main/java/com/example/cocktailapp/DataViewModel.kt
+++ app/src/main/java/com/example/cocktailapp/DataViewModel.kt
@@ -1,4 +1,34 @@
 package com.example.cocktailapp
 
-class DataViewModel {
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class DataViewModel:ViewModel() {
+
+    init {
+        fetchCategories()
+    }
+   private val _categoryState = mutableStateOf(CategoryState())
+    val categoryState= mutableStateOf(_categoryState)
+    fun fetchCategories(){
+        viewModelScope.launch {
+            _categoryState.value=_categoryState.value.copy(loading = true)
+        try {
+            val category= recipeService.getCategories()
+            _categoryState.value=_categoryState.value.copy(loading = false,
+                categoryList = category.drinks,
+                error = null)
+        } catch(e:Exception)
+        {
+            _categoryState.value=_categoryState.value.copy(loading = false,
+                error =e.message)
+        }
+        }
+
+    }
+    data class CategoryState(val loading:Boolean=true,
+                             val categoryList:List<category> = emptyList(),
+                             val error: String?=null)
 }
